0e3daf2631d16be9ceb3c0f1c794714ac654e736
TehekuCoder <j.d.w.w@gmx.de>
2026-03-20 16:17:23 +0100

    Update mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -17,33 +17,22 @@ PROGRAM="NUMBERS"
 export SSHPASS="$SSH_PASSWORD"
 SSH_OPTS="-p 22 -o StrictHostKeyChecking=no -o BatchMode=no"
 
-echo "-> Connect with ${SSH_USERNAME}@${SSH_HOST}..."
-
-# --- Check Java os.name ------------------------------------
-sshpass -e ssh $SSH_OPTS "${SSH_USERNAME}@${SSH_HOST}" \
-  "export JAVA_HOME=/usr/lpp/java/J8.0_64 && \${JAVA_HOME}/bin/java -XshowSettings:property 2>&1 | grep os.name"
-
-# --- Debug: check directory structure on mainframe -------------
-sshpass -e ssh $SSH_OPTS "${SSH_USERNAME}@${SSH_HOST}" \
-  "ls -al ${REMOTE_DIR}/src/main/cobol/ 2>/dev/null || echo 'src/main/cobol/ not found!'"
+echo "-> Connecting to ${SSH_USERNAME}@${SSH_HOST}..."
 
 # --- Generate remote script directly on mainframe --------------
 sshpass -e ssh $SSH_OPTS "${SSH_USERNAME}@${SSH_HOST}" "
 rm -f ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'java -XshowSettings:property 2>&1 | grep os.name' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'LOWERCASE_USERNAME=\$1' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'USERNAME=\$2' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'REMOTE_DIR=\"/z/\${LOWERCASE_USERNAME}/cobolcheck\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'PROGRAM=\"NUMBERS\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'export JAVA_HOME=/usr/lpp/java/J8.0_64' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'export PATH=\"\${JAVA_HOME}/bin:\${PATH}\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cd \"\${REMOTE_DIR}\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'chmod +x scripts/linux_gnucobol_run_tests' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'chmod +x scripts/zos_run_tests' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cd "${REMOTE_DIR}"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'java -Dcobolcheck.test.run=false -jar ${REMOTE_DIR}/bin/cobol-check-0.2.19.jar -p "${PROGRAM}"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cp \"testruns/CC##99.CBL\" \"//\x27\${USERNAME}.CBL(\${PROGRAM})\x27\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cp \"\${PROGRAM}.JCL\" \"//\x27\${USERNAME}.JCL(\${PROGRAM})\x27\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'LOWERCASE_USERNAME=\$1\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'USERNAME=\$2\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'REMOTE_DIR=\"/z/\${LOWERCASE_USERNAME}/cobolcheck\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'PROGRAM=\"NUMBERS\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'export JAVA_HOME=/usr/lpp/java/J8.0_64\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'export PATH=\"\${JAVA_HOME}/bin:\${PATH}\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'cd \"\${REMOTE_DIR}\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'chmod +x scripts/zos_run_tests\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'java -jar \${REMOTE_DIR}/bin/cobol-check-0.2.19.jar -p \"\${PROGRAM}\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'cp \"testruns/CC##99.CBL\" \"//\x27\${USERNAME}.CBL(\${PROGRAM})\x27\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+printf 'cp \"\${PROGRAM}.JCL\" \"//\x27\${USERNAME}.JCL(\${PROGRAM})\x27\"\n' >> ${REMOTE_DIR}/remote_cobolcheck.sh
 chmod +x ${REMOTE_DIR}/remote_cobolcheck.sh
 "
 
