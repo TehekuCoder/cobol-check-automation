c8e848159474704a983af1ddf58087ef77a99aaa
TehekuCoder <j.d.w.w@gmx.de>
2026-03-20 02:17:35 +0100

    Update mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -19,20 +19,17 @@ echo "-> Connect with ${SSH_USERNAME}@${SSH_HOST}..."
 # --- Generate remote script directly on mainframe --------------
 sshpass -e ssh $SSH_OPTS "${SSH_USERNAME}@${SSH_HOST}" "
 rm -f ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'LOWERCASE_USERNAME=\$1' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'USERNAME=\$2' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'REMOTE_DIR=\"/z/\${LOWERCASE_USERNAME}/cobolcheck\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'PROGRAM=\"NUMBERS\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'LOWERCASE_USERNAME=$1' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'USERNAME=$2' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'REMOTE_DIR="/z/${LOWERCASE_USERNAME}/cobolcheck"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'PROGRAM="NUMBERS"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
 echo 'export JAVA_HOME=/usr/lpp/java/J8.0_64' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'export PATH=\"\${JAVA_HOME}/bin:\${PATH}\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cd \"\${REMOTE_DIR}\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'chmod +x cobolcheck' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'export PATH="${JAVA_HOME}/bin:${PATH}"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'cd "${REMOTE_DIR}"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
 echo 'chmod +x scripts/linux_gnucobol_run_tests' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo './cobolcheck -p \"\${PROGRAM}\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cp \"CC##99.CBL\" \"//\x27\${USERNAME}.CBL(\${PROGRAM})\x27\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-echo 'cp \"\${PROGRAM}.JCL\" \"//\x27\${USERNAME}.JCL(\${PROGRAM})\x27\"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
-chmod +x ${REMOTE_DIR}/remote_cobolcheck.sh
-"
+echo 'java -jar ${REMOTE_DIR}/bin/cobol-check-0.2.19.jar -p "${PROGRAM}"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'cp "CC##99.CBL" "//${USERNAME}.CBL(${PROGRAM})"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
+echo 'cp "${PROGRAM}.JCL" "//${USERNAME}.JCL(${PROGRAM})"' >> ${REMOTE_DIR}/remote_cobolcheck.sh
 
 # --- Debug: show mainframe directory structure -----------------
 sshpass -e ssh $SSH_OPTS "${SSH_USERNAME}@${SSH_HOST}" \
